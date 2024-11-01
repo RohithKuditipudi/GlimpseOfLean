ebf5ab3d10819d092d26fdd2821e5f4c4faaffbc
Rohith Kuditipudi <rohith.kuditipudi@gmail.com>
2024-11-01 22:48:18 +0000

    working on lean

--- GlimpseOfLean/Exercises/02Iff.lean
+++ GlimpseOfLean/Exercises/02Iff.lean
@@ -35,7 +35,11 @@ prove one-by-one.
 -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  apply add_pos
+  apply sq_pos_of_pos
+  exact ha
+  apply sq_pos_of_pos
+  exact hb
 }
 
 /-
@@ -60,7 +64,13 @@ example (a : ℝ) (ha : 0 < a) : 0 < (a^2)^2 := by {
 /- Now prove the same lemma as before using forwards reasoning. -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  have h1 : 0 < a^2 := by
+    apply sq_pos_of_pos
+    exact ha
+  have h2: 0 < b^2 := by
+    apply sq_pos_of_pos
+    exact hb
+  apply add_pos h1 h2
 }
 
 
@@ -79,7 +89,12 @@ example (a : ℝ) : a > 0 → b > 0 → a + b > 0 := by {
 /- Now prove the following simple statement in propositional logic.
 Note that `p → q → r` means `p → (q → r)`. -/
 example (p q r : Prop) : (p → q) → (p → q → r) → p → r := by {
-  sorry
+  intro h1 h2 h3
+  have h4 : q := by
+    exact h1 h3
+  have h5 : q → r := by
+    exact h2 h3
+  exact h5 h4
 }
 
 /- # Equivalences
@@ -110,7 +125,9 @@ Let's prove a variation
 -/
 
 example {a b : ℝ} (c : ℝ) : a + c ≤ b + c ↔ a ≤ b := by {
-  sorry
+  rw [← sub_nonneg]
+  ring
+  rw [sub_nonneg]
 }
 
 /-
@@ -148,7 +165,9 @@ example {a b : ℝ}  (ha : 0 ≤ a) : b ≤ a + b := by {
 /- Let's do a variant using `add_le_add_iff_left a : a + b ≤ a + c ↔ b ≤ c` instead. -/
 
 example (a b : ℝ) (hb : 0 ≤ b) : a ≤ a + b := by {
-  sorry
+  calc
+    a = a + 0 := by ring
+    _ ≤ a + b := by exact (add_le_add_iff_left a).2 hb
 }
 
 /-
@@ -179,7 +198,16 @@ example (a b : ℝ) : (a-b)*(a+b) = 0 ↔ a^2 = b^2 := by {
 /- You can try it yourself in this exercise. -/
 
 example (a b : ℝ) : a = b ↔ b - a = 0 := by {
-  sorry
+  constructor
+  · intro h
+    calc
+      b - a = a - a := by rw [h]
+      _ = 0 := by ring
+  · intro h
+    calc
+      a = a + 0 := by ring
+      _ = a + (b - a) := by rw [← h]
+      _ = b := by ring
 }
 
 /-
@@ -190,4 +218,3 @@ equivalences. You learned about tactics:
 * `have`
 * `constructor`
 -/
-
